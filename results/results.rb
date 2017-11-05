ebc10f4f0fdf8681a1021f8de9f1ebfeab911e0a
masterYulian <yulian.mylyan@nltu.lviv.ua>
2017-11-05 17:06:31 +0200

    Task 323 done

--- results/results.rb
+++ results/results.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require '../task560/calculation_task560'
 
 continue = 'y'
--- task323/calculation_task323.rb
+++ task323/calculation_task323.rb
@@ -1,2 +1,26 @@
 class CalculationTask323
+  def initialize
+    @resultData = []
+    @nNumber = 10
+  end
+
+  def getAllDevidesOfNumber(number)
+    data = []
+    (1..number).each do |item|
+      data << item if (number % item).zero?
+    end
+    data
+  end
+
+  def getTask560Result
+    puts 'Enter number n:'
+    @nNumber = gets.chomp.to_i
+    (0..@nNumber - 1).each do |number|
+      @resultData << number if (getAllDevidesOfNumber(number) & getAllDevidesOfNumber(@nNumber)).max == 1
+    end
+    puts "All mutually simple numbers with #{@nNumber} and smaller than it"
+    print @resultData.to_s
+  end
 end
+d = CalculationTask323.new
+d.getTask560Result
