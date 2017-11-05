22f7b04446fd4f78341fec088e26fe0a0a9f96cf
masterYulian <yulian.mylyan@nltu.lviv.ua>
2017-11-05 16:23:43 +0200

    Task 183 done

--- task183/calculation_task183.rb
+++ task183/calculation_task183.rb
@@ -1,36 +1,27 @@
-# frozen_string_literal: true
-
-require 'pry'
-
 class CalculationTask183
   def initialize
     @countOfNumbers = 10
-    @multipleNumber = 0
+    @deviderNumber = 0
     @resultData = 1
   end
 
   def getTask560Result
     puts 'Enter count numbers in sequence: '
-    @countOfNumbers = 10
+    @countOfNumbers = gets.chomp.to_i
     puts 'Enter multiple number: '
-    @multipleNumber = 5
-    (0..@countOfNumbers).each do |_number|
+    @deviderNumber = gets.chomp.to_i
+    puts 'Sequence numbers:'
+    (1..@countOfNumbers).each do |_number|
       sequenceNumber = rand(100) + 1
-      puts sequenceNumber
-      if (sequenceNumber % @multipleNumber).zero?
-        result = @resultData.to_i
-        p @resultData
-        result = result.to_i**sequenceNumber.to_i
-        @resultData = result.to_i
-      end
+      print("#{sequenceNumber} ")
+      @resultData *= sequenceNumber if (sequenceNumber % @deviderNumber).zero?
     end
     if @resultData == 1
-      puts "There are no numbers divisible by number #{@multipleNumber}"
+      puts "\nThere are no numbers divisible by number #{@deviderNumber}"
+    else
+      puts "\nMultiplication of sequence = #{@resultData}"
     end
-  else
-    puts "Products of sequence = #{@resultData}"
   end
 end
-
 c = CalculationTask183.new
 c.getTask560Result
---
+++ task323/calculation_task323.rb
@@ -0,0 +1,2 @@
+class CalculationTask323
+end
